52b532eea2e5ae601abf9d355ed9d7a7ecc5c769
hamamsy <mohamedelhamamsy789@gmail.com>
2017-02-22 13:41:49 +0200

    Completes slideshow

--- tut3_2/js/main.js
+++ tut3_2/js/main.js
@@ -1,20 +1,22 @@
 $(document).foundation();
 $(document).ready(function() {
-    $("#background-slideshow .slide:first").addClass("current");
+    $('#background-slideshow .slide:first').addClass('current');
+    $('#next').click(function(){ return rotateImages(backward=false); });
+    $('#prev').click(function(){ return rotateImages(backward=true); });
     function rotateImages(backward=true) {
-        var oCurrentPhoto = $("#background-slideshow .slide.current");
+
+        var oCurrentPhoto = $('#background-slideshow .slide.current');
         var oNextPhoto = backward ? oCurrentPhoto.prev(): oCurrentPhoto.next();
+
         if (oNextPhoto.length == 0) {
-            oNextPhoto = $("#background-slideshow .slide:"+(backward?"last":"first"));
+            oNextPhoto = $('#background-slideshow .slide:'+(backward?'last':'first'));
         }
 
-        oCurrentPhoto.removeClass("current").addClass("previous");
+        oCurrentPhoto.removeClass('current');
 
-        oNextPhoto.css({opacity: 0.0}).addClass("current").animate({
+        oNextPhoto.css({opacity: 0.0}).addClass('current').animate({
             opacity: 1.0
-        }, 500, function() {
-            oCurrentPhoto.removeClass("previous");
-        });
+        }, 1000, function() {});
     }
 
 });
