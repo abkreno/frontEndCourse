b029449f27dd8651a98200b51b76ef4c268daad9
hamamsy <mohamedelhamamsy789@gmail.com>
2017-02-22 12:52:33 +0200

    Adds slider and styling

--- tut3_2/js/main.js
+++ tut3_2/js/main.js
@@ -1,3 +1,12 @@
 $(document).foundation();
 $(document).ready(function() {
+    var currSlide = 0;
+    var numOfSlides = 4;
+  
+    $("#next").click(function() {
+        $(`#${currSlide}`).animate({
+            width: 'toggle'
+        }, 350);
+        currSlide = (currSlide + 1) % numOfSlides;
+    });
 });
