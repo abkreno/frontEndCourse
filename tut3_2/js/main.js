0ba93c8b06847aae91fa90984a3ba12cf7546a5b
hamamsy <mohamedelhamamsy789@gmail.com>
2017-02-22 13:17:34 +0200

    Adds function to rotateImages

--- tut3_2/js/main.js
+++ tut3_2/js/main.js
@@ -1,12 +1,21 @@
 $(document).foundation();
 $(document).ready(function() {
-    var currSlide = 0;
-    var numOfSlides = 4;
-  
-    $("#next").click(function() {
-        $(`#${currSlide}`).animate({
-            width: 'toggle'
-        }, 350);
-        currSlide = (currSlide + 1) % numOfSlides;
-    });
+    var animateInterval;
+    function rotateImages(backward=true) {
+        var oCurrentPhoto = $("#background-slideshow img.current");
+        var oNextPhoto = backward ? oCurrentPhoto.prev(): oCurrentPhoto.next();
+        if (oNextPhoto.length == 0) {
+            oNextPhoto = $("#background-slideshow img:"+(backward?"last":"first"));
+        }
+
+        oCurrentPhoto.removeClass("current").addClass("previous");
+
+        oNextPhoto.css({opacity: 0.0}).addClass("current").animate({
+            opacity: 1.0
+        }, 500, function() {
+            oCurrentPhoto.removeClass("previous");
+        });
+    }
+    animateInterval = setInterval(rotateImages, 1000);
+
 });
